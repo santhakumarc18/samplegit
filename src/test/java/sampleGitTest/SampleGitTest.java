76680af45b9916a49ce9820bca1e5fe9082f2733
santhakumarc18 <santhakumarc18@gmail.com>
2019-03-16 17:50:14 +0530

    second commit

---
+++ src/test/java/sampleGitTest/SampleGitTest.java
@@ -0,0 +1,25 @@
+package sampleGitTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class SampleGitTest {
+	@Test
+	public void loginTest() throws IOException{
+		WebDriver wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com");
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("C:\\SeleniumWorkspace\\SampleGit\\screenshot\\google.jpg"));
+		
+	}
+
+}
